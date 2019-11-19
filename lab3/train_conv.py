c2b7282da0173229bc8b0aa73b583889f53584c0
Digletto <dat15hbe@student.lu.se>
2019-11-19 18:23:08 +0100

    added use of validation data during training and ImageDataGenerator morphing

--- lab3/train_conv.py
+++ lab3/train_conv.py
@@ -1,12 +1,13 @@
 from keras import layers, models
 import matplotlib.pyplot as plt
+import os
 from keras import layers, models
 from keras.preprocessing.image import ImageDataGenerator
 
 # keras.__version__
 
 
-def train(resize, train_path, batch_size, epochs):
+def train(resize, data_path, batch_size, epochs):
     model = models.Sequential()
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(resize, resize, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
@@ -25,9 +26,25 @@ def train(resize, train_path, batch_size, epochs):
 
     # ------------------------------------------------------------------------------------------------
 
-    train_datagen = ImageDataGenerator(rescale=1. / 255)
+    train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                       rotation_range=40,
+                                       width_shift_range=0.2,
+                                       height_shift_range=0.2,
+                                       shear_range=0.2,
+                                       zoom_range=0.2,
+                                       horizontal_flip=True,
+                                       fill_mode='nearest')
     train_generator = train_datagen.flow_from_directory(
-            train_path,  # This is the source directory for training images
+            os.path.join(data_path, 'train'),  # This is the source directory for training images
+            target_size=(64, 64),  # All images will be resized to target size
+            batch_size=batch_size,
+            # Specify the classes explicitly
+            classes=['daisy', 'dandelion', 'rose', 'sunflower', 'tulip'],
+            # Since we use categorical_crossentropy loss, we need categorical labels
+            class_mode='categorical')
+
+    validation_generator = train_datagen.flow_from_directory(
+            os.path.join(data_path, 'validation'),  # This is the source directory for training images
             target_size=(64, 64),  # All images will be resized to target size
             batch_size=batch_size,
             # Specify the classes explicitly
@@ -37,10 +54,12 @@ def train(resize, train_path, batch_size, epochs):
 
     # ------------------------------------------------------------------------------------------------
 
-    total_sample = train_generator.n
+    # total_sample = train_generator.n
     history = model.fit_generator(
             train_generator,
-            steps_per_epoch=int(total_sample/batch_size),
+            steps_per_epoch=train_generator.samples // batch_size,
+            validation_data=validation_generator,
+            validation_steps=validation_generator.samples // batch_size,
             epochs=epochs,
             verbose=1)
 
@@ -49,7 +68,7 @@ def train(resize, train_path, batch_size, epochs):
     plt.figure(figsize=(7, 4))
     plt.plot([i+1 for i in range(epochs)],history.history['acc'], '-o', c='k', lw=2, markersize=9)
     plt.grid(True)
-    plt.title("Training accuracy with epochs\n",fontsize=18)
+    plt.title("Training accuracy with epochs\n", fontsize=18)
     plt.xlabel("Training epochs", fontsize=15)
     plt.ylabel("Training accuracy", fontsize=15)
     plt.xticks(fontsize=15)
@@ -59,4 +78,4 @@ def train(resize, train_path, batch_size, epochs):
     return history
 
 
-train(64, '../datasets/flowers_split/train', 128, 20)
+train(64, '../datasets/flowers_split/', 128, 20)

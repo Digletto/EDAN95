4b99f5b2e3ef8efaff0e7e1939a13ea110f7e1b1
Kasper Socha <fte10kso@student.lu.se>
2019-12-01 21:38:53 +0100

    Lab 5 - Add stubs for remaining classifiers

---
+++ lab5/naive_bayes.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class NaiveBayes():
+    def fit(self, x, y):
+        pass
+
+    def predict(self, x):
+        print('\033[91mNBC not implemented\033[0m')
+
+        return np.zeros(len(x))

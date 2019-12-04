de4b4c08a2c26999395bcb4b98472df1ed104393
Digletto <dat15hbe@student.lu.se>
2019-12-04 15:18:03 +0100

    naive bayes done, a bit slow but it's also a stupid method

--- lab5/naive_bayes.py
+++ lab5/naive_bayes.py
@@ -2,64 +2,51 @@ import numpy as np
 import operator
 
 
-def gaussian_probability(x, mean, stdev):
-    if stdev == 0 and x == mean:
-        return 1
-    elif stdev == 0 and x != mean:
-        return 0
-    else:
-        exponent = np.exp(-((x - mean) ** 2 / (2 * stdev ** 2)))
-        return (1 / (np.sqrt(2 * np.pi) * stdev)) * exponent
-
-
-def class_probability(label, value, class_meanstd, meanY, stdY):
+def class_probability(label, value, divided, train, class_prob):
     # not the actual probability calculation but maximises the same
     # needs division by overall value probability to be correct but
     # since that's the same division over all classes it doesn't matter for the argmax comparison
     probs = list()
+    divided_matrix = np.stack(divided[label])
+    train_matrix = np.stack(train)
     for i in range(len(value)):
-        probs.append(gaussian_probability(value[i], class_meanstd[i][0], class_meanstd[i][1]))
-    p_class = gaussian_probability(label, meanY, stdY)
+        occurences_total = np.sum((train_matrix[:, i] == value[i]))
+        occurences_in_label = np.sum((divided_matrix[:, i] == value[i]))
+        if occurences_total == 0:
+            probs.append(0.0)
+        else:
+            probs.append(occurences_in_label/occurences_total)
+    p_class = class_prob[label]
 
     return np.prod(probs) * p_class
 
 
 class NaiveBayes:
     def fit(self, x, y):
-        divided = dict()
+        self.class_prob = dict()
+        self.train = x
+        self.divided = dict()
         for i in range(len(x)):
             label = y[i]
             value = x[i]
-            if label not in divided:
-                divided[label] = list()
-            divided[label].append(value)
+            if label not in self.divided:
+                self.divided[label] = list()
+            self.divided[label].append(value)
 
-        self.mean = np.mean(x)
-        self.std = np.std(x)
-        self.meanY = np.mean(y)
-        self.stdY = np.std(y)
+        for label in self.divided:
+            self.class_prob[label] = y.tolist().count(label)/len(y)
 
-        self.meanstd = dict()
-        for label in divided:
-            meanstd_list = list()
-            data_matrix = np.vstack(divided[label])
-            for column in data_matrix.T:
-                meanstd_list.append((np.mean(column), np.std(column)))
-
-            self.meanstd[label] = meanstd_list
-
-    def all_class_probs(self, value):
+    def all_class_probs(self, values):
         class_probs = dict()
-        for label in self.meanstd:
-            class_probs[label] = class_probability(label, value, self.meanstd[label], self.meanY, self.stdY)
+        for label in self.divided:
+            class_probs[label] = class_probability(label, values, self.divided, self.train, self.class_prob)
 
         return class_probs
 
     def predict(self, x):
-        print('\033[91mNBC not implemented\033[0m')
+        print('\033[91mNBC badly implemented\033[0m')
         predictions = list()
         for value in x:
-            # print(value)
             probs = self.all_class_probs(value)
             predictions.append(max(probs.items(), key=operator.itemgetter(1))[0])
 
